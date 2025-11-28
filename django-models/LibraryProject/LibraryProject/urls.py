cde832df49c9cb6ca7a3da520857c7ad41bbb2e9
Agyin-Obeng <agyinobeng60@gmail.com>
2025-11-28 01:54:00 +0000

    Update urls.py

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -9,15 +9,13 @@ urlpatterns = [
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from django.urls import path
-from .views.admin_view import admin_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('relationship_app.urls')),
     path('', RedirectView.as_view(url='/login/', permanent=False)),  # Root redirects to login
-    path('admin/', admin_view, name='admin_view'),
-]
+    path('', include('relationship_app.urls')),
+
 ]
 
 
