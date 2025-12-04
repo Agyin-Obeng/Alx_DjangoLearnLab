582c783d0fe9e50518518ba8620b1587050a0f0f
Agyin-Obeng <agyinobeng60@gmail.com>
2025-12-04 22:26:21 +0000

    Implement PostByTagListView for tag filtering
    Added PostByTagListView to filter posts by tag.

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -207,3 +207,15 @@ def add_comment(request, pk):
         form = CommentForm()
     return render(request, 'blog/comment_form.html', {'form': form})
 
+from django.views.generic import ListView
+from .models import Post
+from taggit.models import Tag
+
+class PostByTagListView(ListView):
+    model = Post
+    template_name = 'blog/posts_by_tag.html'  # create this template
+    context_object_name = 'posts'
+    
+    def get_queryset(self):
+        tag_name = self.kwargs.get('tag_name')
+        return Post.objects.filter(tags__name__iexact=tag_name)
