722eeb99d2f5086533735a2676b3e4e798b43556
Agyin-Obeng <agyinobeng60@gmail.com>
2025-12-15 00:17:03 +0000

    add follow and unfollow view

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -2,15 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    bio = models.TextField(blank=True)
-    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
-
     following = models.ManyToManyField(
-        'self',
+        "self",
         symmetrical=False,
-        related_name='followers',
-        blank=True
+        blank=True,
+        related_name="followers"
     )
+
 
     def __str__(self):
         return self.username
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,15 +1,28 @@
-from rest_framework import generics
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from django.contrib.auth import get_user_model
-from .serializers import RegisterSerializer
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
 
-class RegisterView(generics.CreateAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = RegisterSerializer
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
 
-        return Response(serializer.data)
+    def post(self, request, user_id):
+        user_to_follow = get_object_or_404(CustomUser.objects.all(), id=user_id)
+        request.user.following.add(user_to_follow)
+        return Response(
+            {"detail": "User followed successfully"},
+            status=status.HTTP_200_OK
+        )
+
+
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        user_to_unfollow = get_object_or_404(CustomUser.objects.all(), id=user_id)
+        request.user.following.remove(user_to_unfollow)
+        return Response(
+            {"detail": "User unfollowed successfully"},
+            status=status.HTTP_200_OK
+        )
