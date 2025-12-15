0eeb60c48794d79ff62e0dffd78b1c080152ce50
Agyin-Obeng <agyinobeng60@gmail.com>
2025-12-15 09:06:46 +0000

    update account/urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import RegisterView
-from rest_framework.authtoken.views import obtain_auth_token
+from .views import FollowUserView, UnfollowUserView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', obtain_auth_token, name='login'),
+    path("follow/<int:user_id>/", FollowUserView.as_view(), name="follow-user"),
+    path("unfollow/<int:user_id>/", UnfollowUserView.as_view(), name="unfollow-user"),
 ]
