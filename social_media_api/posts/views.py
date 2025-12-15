93cfbfd4cdc1a3ab69b6cb6c7012af070c17c66e
Agyin-Obeng <agyinobeng60@gmail.com>
2025-12-15 00:20:27 +0000

    add a view in the post app

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,37 +1,16 @@
-from rest_framework import viewsets, permissions, filters
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer  # make sure serializers.py exists in the same folder
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
+from rest_framework import generics, permissions
 from .models import Post
+from .serializers import PostSerializer
 
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
+
+class FeedView(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    # Enable search by title and content
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'content']
+    def get_queryset(self):
+        user = self.request.user
+        following_users = user.following.all()
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by('-created_at')
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def feed(request):
-    followed_users = request.user.following.all()
-    posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
+        return Post.objects.filter(
+            author__in=following_users
+        ).order_by("-created_at")
